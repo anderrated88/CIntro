fd4a9a1dcafbb2dcd87550541ca87b02832a7b32
Thanh An <clgt802@gmail.com>
2021-06-07 11:25:23 +0700

    run test

--- week11/11_5.c
+++ week11/11_5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void swap(int x, int y, int z)
+void swap(int *x, int *y, int *z)
 {
     int temp = *x;
     *x = *y;
@@ -10,15 +10,14 @@ void swap(int x, int y, int z)
 int main()
 {
     int x, y, z;
-    int *p, *q, *r;
-    printf("Enter x,y,z:");
+    int *p, *q, *r,*temp;
+    printf("Enter x,y,z:\n");
     scanf("%d%d%d", &x, &y, &z);
     p = &x;
     q = &y, r = &z;
     printf("The values of x y z are %d %d %d\n", x, y, z);
     printf("The value of p q r are %p %p %p\n", p, q, r);
     printf("The value of *p *q *r are %d %d %d\n", *p, *q, *r);
-    int temp;
     swap(p, q, r);
     printf("The values of x y z are %d %d %d\n", x, y, z);
     printf("The value of p q r are %p %p %p\n", p, q, r);
